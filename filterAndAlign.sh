6076d30a7a22dcf318477b1b0c0f7b3cc78eba71
Sergey Koren <sergek@umd.edu>
2018-07-19 12:35:20 -0400

    Keep bam formatting when filtering unmapped reads

--- filterAndAlign.sh
+++ filterAndAlign.sh
@@ -98,7 +98,7 @@ fi
 echo "Mapping $prefix $line to $reference"
 
 samtools bam2fq -n $line |gzip -c > $jobid.fastq.gz
-minimap2 -ax map-pb   -t $cores --secondary=no $prefix.mmi $jobid.fastq.gz |samtools view -bT $reference - |  samtools view -F 0x04 - > $jobid.minimap.bam
+minimap2 -ax map-pb   -t $cores --secondary=no $prefix.mmi $jobid.fastq.gz |samtools view -bT $reference - |  samtools view -bF 0x04 - > $jobid.minimap.bam
 pbbamify --input=$jobid.minimap.bam --output $prefix.$jobid.unsorted.bam $reference $line
 samtools sort -m 4G -@ $cores -T ./$jobid.tmp $prefix.$jobid.aln.bam $prefix.$jobid.unsorted.bam
 pbindex $prefix.$jobid.aln.bam
